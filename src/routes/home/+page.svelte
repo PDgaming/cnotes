cf6e600f55470ac9074bc61179312bf77640fb90
PD <89239979+PDgaming@users.noreply.github.com>
2024-12-11 21:39:20 +0530

    removed share option from home page

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -327,29 +327,6 @@
                 <ul
                   class="menu dropdown-content bg-base-100 rounded-box z-[1] w-20 p-2 shadow options"
                 >
-                  <li class="mb-2">
-                    <button
-                      class="btn btn-success"
-                      on:click={() => {
-                        my_modal_4.showModal();
-                      }}
-                      >Share
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        class="size-6"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z"
-                        />
-                      </svg>
-                    </button>
-                  </li>
                   <li>
                     <button
                       class="btn btn-error"
@@ -403,20 +380,6 @@
                       >Delete
                     </button>
                   </div>
-                </div>
-              </dialog>
-              <dialog id="my_modal_4" class="modal">
-                <div class="modal-box">
-                  <form method="dialog">
-                    <button
-                      class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
-                      >✕</button
-                    >
-                  </form>
-                  <label>Link:</label>
-                  <a href="/home/{note.slug}/sharing" class="share-link"
-                    >https://cnotes.pages.dev/{note.slug}/sharing</a
-                  >
                 </div>
               </dialog>
             </div>
